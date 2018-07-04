9c85a9099f877803050f44de93a68ec2aa7eeefe
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2018-07-04 11:52:06 +0200

    Moved ViewModel files to ViewModel folder, Extracted fat classes to seperate classes

---
+++ Vidly/ViewModels/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.ViewModels
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Driving Licens")]
+        public string DrivingLicens { get; set; }
+        [Required]
+        public string Phone { get; set; }
+    }
+}
